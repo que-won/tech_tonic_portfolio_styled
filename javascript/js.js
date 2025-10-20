eadccd75ce483ca8a3d659e9f627c94befe436ee
Tech Tonic <archiequewon921@gmail.com>
2025-10-20 06:45:30 +0000

    update

---
+++ javascript/js.js
@@ -0,0 +1,11 @@
+ tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#032B44",
+          accent: "#D4A017",
+          orange: "#E88C00",
+        },
+      },
+    },
+  };
